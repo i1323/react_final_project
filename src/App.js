7b5c088565e04bc3ab2011082fd8f484c1b40b0a
i1323 <64890877+i1323@users.noreply.github.com>
2020-08-13 21:37:37 +0200

    added Github Repo Link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Maggie's Weather App</h1>
+      <footer>
+        This project was coded by Maggie and is{" "}
+        <a href="https://github.com/i1323/react_final_project" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
