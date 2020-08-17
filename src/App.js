ced60e566ccaf61270bd82a8429d801f524f2860
i1323 <64890877+i1323@users.noreply.github.com>
2020-08-17 15:02:13 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Maggie's Weather App</h1>
-      <footer>
-        This project was coded by Maggie and is{" "}
-        <a href="https://github.com/i1323/react_final_project" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Maggie's Weather App</h1>
+        <footer>
+          This project was coded by Maggie and is{" "}
+          <a
+            href="https://github.com/i1323/react_final_project"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
